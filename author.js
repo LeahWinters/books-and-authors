fa50df318861ea9e57dca225f3bfa7693df3c3bc
Leah Winters <leah.winters24@gmail.com>
2020-01-05 12:46:40 -0700

    Passed all tests

--- author.js
+++ author.js
@@ -1,5 +1,16 @@
-class Author {
+var Book = require('./book');
 
+class Author {
+  constructor(authorInfo) {
+    this.first = authorInfo.first;
+    this.last = authorInfo.last;
+    this.books = [];
+  }
+  write(title, publicationYear) {
+    var aNewBook = new Book(this.first, this.last, title, publicationYear);
+    this.books.push(aNewBook);
+    return aNewBook;
+  }
 }
 
 module.exports = Author;
